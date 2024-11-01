44728ff8e5f1e7bfce142139122635ceccaaf78a
araki-shota2351780 <araki.shota0227@gmail.com>
2024-11-01 13:45:59 +0000

    home_update

--- home/home.js
+++ home/home.js
@@ -10,4 +10,15 @@ document.addEventListener("DOMContentLoaded", function() {
             link.classList.add("active");
         }
     });
+
+    // 星をランダムに配置
+    const body = document.querySelector('body');
+    for (let i = 0; i < 100; i++) {
+        const star = document.createElement('div');
+        star.classList.add('star');
+        star.style.top = Math.random() * 100 + 'vh';
+        star.style.left = Math.random() * 100 + 'vw';
+        star.style.animationDuration = (Math.random() * 2 + 1) + 's';
+        body.appendChild(star);
+    }
 });
