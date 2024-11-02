c4271bc8c609d30ab01d5acd8d401067381d841c
araki-shota2351780 <araki.shota0227@gmail.com>
2024-11-02 12:45:45 +0000

    test.dev

--- scripts.js
+++ scripts.js
@@ -1,21 +1,22 @@
 export default {
     async fetch(request, env) {
-      const { pathname } = new URL(request.url);
-  
-      if (pathname === "/api/beverages") {
-        // If you did not use `DB` as your binding name, change it here
-        const { results } = await env.DB.prepare(
-          "SELECT * FROM Customers WHERE CompanyName = ?"
-        )
-          .bind("Bs Beverages")
-          .all();
-        return new Response(JSON.stringify(results), {
-          headers: { 'Content-Type': 'application/json' }
-        });
-      }
-  
-      return new Response(
-        "Call /api/beverages to see everyone who works at Bs Beverages"
-      );
+        const { pathname } = new URL(request.url);
+
+        if (pathname === "/api/beverages") {
+            // 'DB'はwrangler.tomlで定義したデータベースバインディング名と一致する必要があります
+            const { results } = await env.DB.prepare(
+                "SELECT * FROM Customers WHERE CompanyName = ?"
+            )
+            .bind("Bs Beverages")  // SQL文のパラメータをバインド
+            .all();
+
+            return new Response(JSON.stringify(results), {
+                headers: { 'Content-Type': 'application/json' }  // JSON形式のレスポンス
+            });
+        }
+
+        return new Response(
+            "Call /api/beverages to see everyone who works at Bs Beverages"  // その他のパスへのデフォルトレスポンス
+        );
     },
-  };
+};
