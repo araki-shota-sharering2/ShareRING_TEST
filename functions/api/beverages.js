8033f6483bcf10cf0dffffed6c89782fdd8820fc
araki-shota2351780 <araki.shota0227@gmail.com>
2024-11-02 14:07:31 +0000

    allreset

--- functions/api/beverages.js
+++
@@ -1,8 +0,0 @@
-export default {
-    async fetch(request) {
-        const data = { message: "This is a JSON response" };
-        return new Response(JSON.stringify(data), {
-            headers: { 'Content-Type': 'application/json' }
-        });
-    }
-};
