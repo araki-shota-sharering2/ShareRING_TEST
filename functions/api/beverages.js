e241cd331441fd89b19951cc184ce7354ca3ae28
araki-shota2351780 <araki.shota0227@gmail.com>
2024-11-02 13:08:19 +0000

    change

--- functions/api/beverages.js
+++ functions/api/beverages.js
@@ -1,9 +1,9 @@
 export default {
     async fetch(request) {
         const data = [
-            { id: 1, name: "Bs Beverages", location: "Tokyo" }
-            // 他のデータ
-        ];
+            { id: 1, name: "Bs Beverages", location: "Tokyo" },
+            { id: 2, name: "Another Drink", location: "Osaka" }
+        ]; // ダミーデータ
 
         return new Response(JSON.stringify(data), {
             headers: { 'Content-Type': 'application/json' }
