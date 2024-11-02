e0b3de066ba01bf4fcf5aac908f3e03675bbab7b
araki-shota2351780 <araki.shota0227@gmail.com>
2024-11-02 16:44:08 +0000

    fc

--- functions/fetch-data.js
+++ functions/fetch-data.js
@@ -1,13 +1,7 @@
 export default {
     async fetch(request, env) {
-        try {
-            // 簡単なテストレスポンス
-            return new Response(JSON.stringify({ message: "Hello, this is a test response" }), {
-                headers: { "Content-Type": "application/json" },
-            });
-        } catch (error) {
-            console.error("Error in function:", error);
-            return new Response("Internal Server Error", { status: 500 });
-        }
+        return new Response(JSON.stringify({ message: "Hello, this is a test response" }), {
+            headers: { "Content-Type": "application/json" },
+        });
     }
 };
