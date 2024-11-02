b5ae44cd272de00d1a849c943014e0e6add29d50
araki-shota2351780 <araki.shota0227@gmail.com>
2024-11-02 14:01:43 +0000

    change

--- functions/api/beverages.js
+++ functions/api/beverages.js
@@ -1,21 +1,8 @@
 export default {
-    async fetch(request, env) {
-        const { pathname } = new URL(request.url);
-
-        if (pathname === "/api/beverages") {
-            // ダミーデータを作成
-            const data = [
-                { id: 1, name: "Bs Beverages", location: "Tokyo" },
-                { id: 2, name: "Another Drink", location: "Osaka" }
-            ];
-
-            // JSONレスポンスを返す
-            return new Response(JSON.stringify(data), {
-                headers: { 'Content-Type': 'application/json' }
-            });
-        }
-
-        // 指定されたパス以外へのリクエストの場合、404を返す
-        return new Response("Not Found", { status: 404 });
+    async fetch(request) {
+        const data = { message: "This is a JSON response" };
+        return new Response(JSON.stringify(data), {
+            headers: { 'Content-Type': 'application/json' }
+        });
     }
 };
