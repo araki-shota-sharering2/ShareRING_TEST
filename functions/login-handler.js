b09b99be05e84eb839089ba1edc040b845574b09
araki-shota2351780 <araki.shota0227@gmail.com>
2024-11-11 09:55:01 +0000

    return

--- functions/login-handler.js
+++ functions/login-handler.js
@@ -1,27 +1,23 @@
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-
-const SECRET_KEY = process.env.SECRET_KEY;
-
 export async function onRequestPost(context) {
     const { request, env } = context;
     const { email, password } = await request.json();
+
     const db = env.DB;
 
     try {
-        const user = await db.prepare('SELECT * FROM user_accounts WHERE email = ?').bind(email).first();
+        // ユーザー情報の照会
+        const user = await db.prepare('SELECT * FROM user_accounts WHERE email = ? AND password = ?')
+            .bind(email, password)
+            .first();
 
-        if (user && await bcrypt.compare(password, user.password)) {
-            const token = jwt.sign({ userId: user.user_id, email: user.email }, SECRET_KEY, { expiresIn: '1h' });
-
+        if (user) {
+            // ログイン成功
             return new Response(JSON.stringify({ message: 'ログイン成功' }), {
                 status: 200,
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Set-Cookie': `token=${token}; HttpOnly; Path=/; Max-Age=3600`
-                }
+                headers: { 'Content-Type': 'application/json' }
             });
         } else {
+            // 認証エラー
             return new Response(JSON.stringify({ message: 'メールアドレスまたはパスワードが間違っています' }), {
                 status: 401,
                 headers: { 'Content-Type': 'application/json' }
