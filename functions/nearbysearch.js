d537e741871fe35302810974b7afa9b4d93c9477
araki-shota2351780 <araki.shota0227@gmail.com>
2024-11-30 05:14:25 +0000

    css

--- functions/nearbysearch.js
+++ functions/nearbysearch.js
@@ -4,7 +4,7 @@ async function handleRequest(req, res) {
     const { location, radius, type } = req.query;
 
     try {
-        const apiResponse = await fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=${radius}&type=${type}&key=YOUR_API_KEY`);
+        const apiResponse = await fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=${radius}&type=${type}&key=AIzaSyCIbW8SaZBjgKXB3yt7ig0OYnzD0TIi2h8`);
         const data = await apiResponse.json();
         res.json(data); // JSONを返す
     } catch (error) {
