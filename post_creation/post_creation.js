46eab2af72732d4227f74b23bd01eafa1682ef3e
araki-shota2351780 <araki.shota0227@gmail.com>
2024-12-01 03:10:05 +0000

    cs

--- post_creation/post_creation.js
+++ post_creation/post_creation.js
@@ -97,4 +97,15 @@ function setupShareButton() {
             alert("投稿に失敗しました。");
         }
     });
+            // 星をランダムに配置
+            const body = document.querySelector('body');
+            for (let i = 0; i < 100; i++) {
+                const star = document.createElement('div');
+                star.classList.add('star');
+                star.style.top = Math.random() * 100 + 'vh';
+                star.style.left = Math.random() * 100 + 'vw';
+                star.style.animationDuration = (Math.random() * 2 + 1) + 's';
+                body.appendChild(star);
+            }
+    
 }
