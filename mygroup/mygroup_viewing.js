1cfc2ab4c0330d05dcc200ab6557054ce4bb38c2
araki-shota2351780 <araki.shota0227@gmail.com>
2024-12-04 13:04:36 +0000

    css

--- mygroup/mygroup_viewing.js
+++ mygroup/mygroup_viewing.js
@@ -234,24 +234,10 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     // もっと見るボタンのクリックイベント
     loadMoreButton.addEventListener("click", () => {
-        if (!loadMoreButton.classList.contains("disabled")) {
-            currentPage++;
-            fetchPosts(currentPage);
-        }
+        currentPage++;
+        fetchPosts(currentPage);
     });
 
-    // 星をランダムに配置
-    const body = document.querySelector("body");
-    for (let i = 0; i < 100; i++) {
-        const star = document.createElement("div");
-        star.classList.add("star");
-        star.style.top = Math.random() * 100 + "vh";
-        star.style.left = Math.random() * 100 + "vw";
-        star.style.animationDuration = Math.random() * 2 + 1 + "s";
-        body.appendChild(star);
-    }
-
-    // 初期データを読み込み
-    await fetchGroupName();
-    await fetchPosts(currentPage);
+    fetchGroupName(); // グループ名を取得
+    fetchPosts(currentPage); // 最初の投稿を取得
 });
