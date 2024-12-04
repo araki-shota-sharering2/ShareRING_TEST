e6f97fc05834b292af4266bedc7e75129c5e3085
araki-shota2351780 <araki.shota0227@gmail.com>
2024-12-04 11:33:07 +0000

    css

--- mymap/mymap.js
+++ mymap/mymap.js
@@ -120,14 +120,5 @@ window.initMap = async function () {
         throw new Error("無効なPOINT形式のデータです: " + pointString);
     }
 
-        // 星をランダムに配置
-        const body = document.querySelector('body');
-        for (let i = 0; i < 100; i++) {
-            const star = document.createElement('div');
-            star.classList.add('star');
-            star.style.top = Math.random() * 100 + 'vh';
-            star.style.left = Math.random() * 100 + 'vw';
-            star.style.animationDuration = (Math.random() * 2 + 1) + 's';
-            body.appendChild(star);
-        }
+
 };
