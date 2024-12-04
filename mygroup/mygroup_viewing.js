262c3c8f9b8511384bd5299adc81d97206d14856
araki-shota2351780 <araki.shota0227@gmail.com>
2024-12-04 10:14:49 +0000

    css

--- mygroup/mygroup_viewing.js
+++ mygroup/mygroup_viewing.js
@@ -66,7 +66,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                 <div class="post-content">
                     <div class="post-frame-wrapper">
                         <img src="${post.image_url}" alt="投稿画像" class="post-image" style="border-color: ${ringColor};">
-                        
+                        <img src="/assets/images/main/ring_keeper.svg" alt="Keep画像" class="keep-image" data-post-id="${post.group_post_id}">
                     </div>
                     <div class="post-details">
                         <div class="user-info">
@@ -76,9 +76,6 @@ document.addEventListener("DOMContentLoaded", async () => {
                         </div>
                         <p class="post-comment">${post.caption || "コメントなし"}</p>
                         <p class="post-date">投稿日: ${new Date(post.created_at).toLocaleDateString()}</p>
-                    </div>
-                    <div class="post-actions">
-                        <div class="swipe-guide">↑ スワイプしてルート案内を開始</div>
                     </div>
                 </div>
             `;
